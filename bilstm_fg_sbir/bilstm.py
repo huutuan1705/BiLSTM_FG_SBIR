7325feaaaf1563b1de079185be8b9d54b2ec7167
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-06 15:55:07 +0700

    add model and update bilstm, backbones, dataset

--- bilstm_fg_sbir/bilstm.py
+++ bilstm_fg_sbir/bilstm.py
@@ -1,7 +1,10 @@
+import torch 
 import torch.nn as nn
 
+from attention import AttentionSequence
+
 class BiLSTM(nn.Module):
-    def __init__(self, input_size, num_layers, output_size, hidden_size = 512, bidirectional=False):
+    def __init__(self, input_size, num_layers, output_size, hidden_size = 512, bidirectional=True):
         super(BiLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
@@ -16,11 +19,15 @@ class BiLSTM(nn.Module):
     def forward(self, x):
         x, _ = self.lstm(x)
         x = self.fc(x)
-        x = x.squeeze(0)
-        return x
 
-# x = torch.randn(1, 25, 2048)
+        attention = AttentionSequence(input_size=x.shape[1], hidden_layer=x.shape[1])
+        output, _ = attention(x)
+        
+        return output
+
+
+# x = torch.randn(25, 2048)
 # model = BiLSTM(input_size=2048, num_layers=1, output_size=64, bidirectional=True)
 # output = model(x)
-# output = output.squeeze(0)
-# print(output.shape) --> output: [25, 64]
+# output = output
+# print(output.shape) 
--- bilstm_fg_sbir/dataset.py
+++ bilstm_fg_sbir/dataset.py
@@ -5,12 +5,14 @@ from PIL import Image
 from random import randint
 from torch.utils.data import Dataset
 from utils import get_transform
+from rasterize import rasterize_sketch
 
 class FGSBIR_Dataset(Dataset):
-    def __init__(self, args, mode):
+    def __init__(self, args, mode, on_fly=False):
         super().__init__()
         self.args = args
         self.mode = mode
+        self.on_fly = on_fly
         
         coordinate_path = os.path.join(args.root_dir, args.dataset_name, args.dataset_name + '_Coordinate')
         self.root_dir = os.path.join(args.root_dir, args.dataset_name)
@@ -50,8 +52,12 @@ class FGSBIR_Dataset(Dataset):
             
             # List sketch image
             # ========================
-            
-            
+            list_sketch_imgs = rasterize_sketch(vector_x)
+            if self.on_fly:
+                sketch_imgs = [self.train_transform(Image.fromarray(sk_img).convert("RGB")) for sk_img in list_sketch_imgs]
+
+            else:
+                sketch_imgs = self.train_transform(Image.fromarray(list_sketch_imgs[-1]).convert("RGB"))
             # ========================
             
             positive_image = Image.open(positive_path).convert("RGB")
@@ -60,3 +66,29 @@ class FGSBIR_Dataset(Dataset):
             positive_image = self.train_transform(positive_image)
             negative_image = self.train_transform(negative_image)
             
+            sample = {
+                'sketch_imgs': sketch_imgs, 'sketch_path': sketch_path,
+                'positive_img': positive_image, 'positive_path': positive_path,
+                'negative_img': negative_image, 'negative_path': negative_sample 
+            }
+        
+        else:
+            sketch_path = self.test_sketch[item]
+            positive_sample = '_'.join(self.test_sketch[item].split('/')[-1].split('_')[:-1])
+            positive_path = os.path.join(self.root_dir, 'photo', positive_sample + '.png')
+            positive_image = self.test_transform(Image.open(positive_path).convert("RGB"))
+            
+            vector_x = self.coordinate[sketch_path]
+            list_sketch_imgs = rasterize_sketch(vector_x)
+            if self.on_fly:
+                sketch_imgs = [self.test_transform(Image.fromarray(sk_img).convert("RGB")) for sk_img in list_sketch_imgs]
+
+            else:
+                sketch_imgs = self.test_transform(Image.fromarray(list_sketch_imgs[-1]).convert("RGB"))
+                
+            sample = {
+                'sketch_imgs': sketch_imgs, 'sketch_path': sketch_path,
+                'positive_img': positive_image, 'positive_path': positive_path,
+            }
+            
+        return sample
