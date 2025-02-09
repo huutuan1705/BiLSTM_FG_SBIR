4b9256f26572a96a5daf41c768e4020cfd9aefec
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-09 09:54:19 +0700

    update backbones

--- bilstm_fg_sbir/backbones.py
+++ bilstm_fg_sbir/backbones.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 from attention import AttentionImage
 from torchvision.models import Inception_V3_Weights, ResNet50_Weights, VGG16_Weights
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 class ResNet50(nn.Module):
     def __init__(self, args):
         super(ResNet50, self).__init__()
@@ -100,7 +102,7 @@ class InceptionV3(nn.Module):
         x = self.Mixed_7c(x)
          
         attention = AttentionImage(input_size=x.shape[1], hidden_layer=x.shape[1])
-        output, _ = attention(x)
+        output, _ = attention(x).to(device)
         
         return output
     
