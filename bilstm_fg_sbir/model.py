1cf74333fd1f0c4dcb01a02ea2a929705e331572
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-09 12:48:23 +0700

    update model

--- bilstm_fg_sbir/model.py
+++ bilstm_fg_sbir/model.py
@@ -17,7 +17,12 @@ class BiLSTM_FGSBIR_Model(nn.Module):
         self.sample_train_params = self.sample_embedding_network.parameters()
         self.optimizer = optim.Adam(self.sample_train_params, args.learning_rate)
         self.args = args
-        
+    
+    def test_forward(self, batch):            #  this is being called only during evaluation
+        sketch_feature = self.sample_embedding_network(batch['sketch_img'].to(device))
+        positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
+        return sketch_feature.cpu(), positive_feature.cpu()
+    
     def train_model(self, batch):
         self.train()
         self.optimizer.zero_grad()
