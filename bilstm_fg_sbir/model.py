3ec7d76c3017bd3b0652d389d4fcdce803dc7083
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-09 21:51:30 +0700

    update model

--- bilstm_fg_sbir/model.py
+++ bilstm_fg_sbir/model.py
@@ -52,9 +52,9 @@ class BiLSTM_FGSBIR_Model(nn.Module):
                         output_size=self.args.output_size).to(device)
         sketch_features = bilstm(sketch_features)
         
-        print("Sketch feature shape: ", sketch_features.shape)
-        print("Positive feature shape: ", positive_feature.shape)
-        print("Negative feature shape: ", negative_feature.shape)
+        # print("Sketch feature shape: ", sketch_features.shape)
+        # print("Positive feature shape: ", positive_feature.shape)
+        # print("Negative feature shape: ", negative_feature.shape)
         
         loss = self.loss(sketch_features, positive_feature, negative_feature)
         loss.backward()
