2c8bf47a242c18e38d624ad371a20bff3bc94bcd
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-15 10:46:08 +0700

    update attention and main

--- bilstm_fg_sbir/bilstm.py
+++ bilstm_fg_sbir/bilstm.py
@@ -1,28 +1,25 @@
 import torch 
 import torch.nn as nn
-
-from attention import AttentionSequence
+import torch.nn.functional as F
 
 class BiLSTM(nn.Module):
-    def __init__(self, input_size=2048, num_layers=1, hidden_size1 = 512, hidden_size2 = 32, bidirectional=True):
+    def __init__(self, args, input_size=2048, bidirectional=True):
         super(BiLSTM, self).__init__()
+        self.args = args
         self.input_size = input_size
-        self.hidden_size1 = hidden_size1
-        self.hidden_size2 = hidden_size2
-        self.num_layers = num_layers
-        self.bilstm1 = nn.LSTM(input_size=self.input_size, hidden_size=self.input_size // 2, num_layers=self.num_layers,
-                            batch_first=True, bidirectional=bidirectional)
-        self.bilstm2 = nn.LSTM(input_size=self.input_size, hidden_size=self.input_size // 2, num_layers=self.num_layers,
+        self.hidden_size1 = args.hidden_size
+        self.num_layers = args.num_layers
+        self.num_bilstm_blocks = args.num_bilstm_blocks
+        self.bilstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers,
                             batch_first=True, bidirectional=bidirectional)
     def forward(self, x):
-        x, _ = self.bilstm1(x)
-        x, _ = self.bilstm2(x)
-        
+        for _ in range(self.num_bilstm_blocks):
+            x = self.bilstm(x)
+            
+        x = x[:, -1, :]       
         return x
 
-
 # x = torch.randn(48, 25, 2048)
-# model = BiLSTM(input_size=2048, num_layers=2, bidirectional=True)
-# output = model(x)
-# output = output
-# print(output.shape) # [48, 25, 64]
+# model = BiLSTM(input_size=2048, hidden_size1=1024, num_layers=2, bidirectional=True)
+# x = model(x)
+# print(x.shape)
