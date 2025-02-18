f826245b2137511c952d7304b20b89ebd5c485c4
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-18 17:00:02 +0700

    update bilstm

--- bilstm_fg_sbir/bilstm.py
+++ bilstm_fg_sbir/bilstm.py
@@ -20,9 +20,9 @@ import torch.nn.functional as F
 #         return x
     
 class BiLSTM(nn.Module):
-    def __init__(self, args, input_dim=2048, hidden_dim1=512, hidden_dim2=32, output_dim=64):
+    def __init__(self, args, input_size=2048, hidden_dim1=512, hidden_dim2=32, output_dim=64):
         super(BiLSTM, self).__init__()
-        self.bilstm1 = nn.LSTM(input_dim, hidden_dim1, batch_first=True, bidirectional=True)
+        self.bilstm1 = nn.LSTM(input_size, hidden_dim1, batch_first=True, bidirectional=True)
         self.bilstm2 = nn.LSTM(hidden_dim1 * 2, hidden_dim2, batch_first=True, bidirectional=True)
         self.attention = nn.Linear(hidden_dim2 * 2, output_dim)
 
