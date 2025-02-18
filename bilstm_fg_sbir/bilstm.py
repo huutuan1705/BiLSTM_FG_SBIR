22607d278dcfaa30f2b79ea7ca07e9cbf054832d
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-18 15:17:59 +0700

    update model

--- bilstm_fg_sbir/bilstm.py
+++ bilstm_fg_sbir/bilstm.py
@@ -2,9 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from attention import AttentionSequence, Linear_global
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 class BiLSTM(nn.Module):
     def __init__(self, args, input_size=2048, bidirectional=True):
         super(BiLSTM, self).__init__()
@@ -13,21 +10,11 @@ class BiLSTM(nn.Module):
         self.hidden_size = args.hidden_size
         self.num_layers = args.num_layers
         self.num_bilstm_blocks = args.num_bilstm_blocks
-        # self.num_layers = 2
-        # self.hidden_size = 1024
-        # self.num_bilstm_blocks = 2
         self.bilstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers,
                             batch_first=True, bidirectional=bidirectional)
-
     def forward(self, x):
         for _ in range(self.num_bilstm_blocks):
-            x, _ = self.bilstm(x) # (N, 25, 2048)
-        
-        # x = AttentionSequence().to(device)(x)
-        
-        return x[:, -1, :] # (N, 25, 2048)  
-
-# x = torch.randn(48, 25, 2048)
-# model = BiLSTM(None, input_size=2048)
-# x = model(x)
-# print(x[-1].shape) # (N, 25, 64)
+            x, _ = self.bilstm(x)
+            
+        x = x[:, -1, :]       
+        return x
