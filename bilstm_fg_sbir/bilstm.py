1c7ea1931060968760467e276ad12e6ae92137d3
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-21 21:28:56 +0700

    update model

--- bilstm_fg_sbir/bilstm.py
+++ bilstm_fg_sbir/bilstm.py
@@ -2,35 +2,33 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from attention import AttentionSequence, Linear_global
-
-class BiLSTM(nn.Module):
-    def __init__(self, args, input_size=2048, bidirectional=True):
-        super(BiLSTM, self).__init__()
-        self.args = args
-        self.input_size = input_size
-        self.hidden_size = args.hidden_size
-        self.num_layers = args.num_layers
-        self.num_bilstm_blocks = args.num_bilstm_blocks
-        
-        # self.hidden_size = 1024
-        # self.num_layers = 1
-        # self.num_bilstm_blocks = 2
-        self.bilstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers,
-                            batch_first=True, bidirectional=bidirectional)
-        self.attention = AttentionSequence()
-        self.bilstm1 = nn.LSTM(input_size=self.input_size, hidden_size=512, num_layers=self.num_layers,
-                            batch_first=True, bidirectional=bidirectional)
-        self.bilstm2 = nn.LSTM(input_size=1024, hidden_size=32, num_layers=self.num_layers,
-                            batch_first=True, bidirectional=bidirectional)
-        
-    def forward(self, x):
-        x, _ = self.bilstm1(x)   
-        x, _ = self.bilstm2(x)   
-        return x
+# class BiLSTM(nn.Module):
+#     def __init__(self, args, input_size=2048, bidirectional=True):
+#         super(BiLSTM, self).__init__()
+#         self.args = args
+#         self.input_size = input_size
+#         self.hidden_size = args.hidden_size
+#         self.num_layers = args.num_layers
+#         self.num_bilstm_blocks = args.num_bilstm_blocks
+#         self.bilstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, num_layers=self.num_layers,
+#                             batch_first=True, bidirectional=bidirectional)
+#     def forward(self, x):
+#         for _ in range(self.num_bilstm_blocks):
+#             x, _ = self.bilstm(x)
+            
+#         x = x[:, -1, :]       
+#         return x
     
+class BiLSTM(nn.Module):
+    def __init__(self, args, input_size=2048, hidden_dim1=512, hidden_dim2=32, output_dim=64):
+        super(BiLSTM, self).__init__()
+        self.bilstm1 = nn.LSTM(input_size, hidden_dim1, batch_first=True, bidirectional=True)
+        self.bilstm2 = nn.LSTM(hidden_dim1 * 2, hidden_dim2, batch_first=True, bidirectional=True)
+        self.attention = nn.Linear(hidden_dim2 * 2, output_dim)
 
-# model = BiLSTM(None)
-# dummy_input = torch.randn(1, 25, 2048)
-# output = model(dummy_input)
-# print(output.shape)  # (1, 25, 64)
+    def forward(self, x):
+        x, _ = self.bilstm1(x)  # (batch, 25, 1024)
+        x, _ = self.bilstm2(x)  # (batch, 25, 64)
+        
+        # x = x[:, -1, :]       
+        return x
--- bilstm_fg_sbir/model.py
+++ bilstm_fg_sbir/model.py
@@ -25,6 +25,7 @@ class BiLSTM_FGSBIR_Model(nn.Module):
         self.sketch_embedding_network.fix_weights()
             
         self.bilstm_network = BiLSTM(args=args, input_size=2048).to(device)
+        # self.bilstm_network.apply(init_weights)
         self.bilstm_params = self.bilstm_network.parameters()
         
         self.attention = Attention_global()
@@ -42,19 +43,7 @@ class BiLSTM_FGSBIR_Model(nn.Module):
         self.optimizer = optim.Adam([
             {'params': self.bilstm_network.parameters(), 'lr': args.learning_rate},
         ])
-    
-    def compute_loss(self, anchors, positive, negative):  
-        positive_expanded = positive.unsqueeze(1).expand(-1, self.args.num_anchors, -1)
-        negative_expanded = negative.unsqueeze(1).expand(-1, self.args.num_anchors, -1)
         
-        anchors_reshaped = anchors.reshape(-1, self.args.output_size)
-        positive_reshaped = positive_expanded.reshape(-1, self.args.output_size)
-        negative_reshaped = negative_expanded.reshape(-1, self.args.output_size)
-        
-        loss = self.loss(anchors_reshaped, positive_reshaped, negative_reshaped)
-        
-        return loss
-    
     def train_model(self, batch):
         self.train()
         self.optimizer.zero_grad()
@@ -62,28 +51,20 @@ class BiLSTM_FGSBIR_Model(nn.Module):
         positive_feature = self.sample_embedding_network(batch['positive_img'].to(device))
         negative_feature = self.sample_embedding_network(batch['negative_img'].to(device))
         
-        positive_feature = self.linear(self.attention(positive_feature)) # (N, 64)
-        negative_feature = self.linear(self.attention(negative_feature)) # (N, 64)
+        positive_feature = self.linear(self.attention(positive_feature)).unsqueeze(1) # (N, 1, 64)
+        negative_feature = self.linear(self.attention(negative_feature)).unsqueeze(1) # (N, 1, 64)
         
-        sketch_imgs_tensor = batch['sketch_imgs']# (N, 25 3, 299, 299)
+        sketch_imgs_tensor = batch['sketch_imgs'] # (N, 25 3, 299, 299)
         sketch_features = []
-        loss = 0
         for i in range(sketch_imgs_tensor.shape[0]):
-            sketch_feature = self.sketch_embedding_network(sketch_imgs_tensor[i].to(device)) # (25, 2048, 8, 8)
-            sketch_feature = self.sketch_attention(sketch_feature) # (25, 2048)
-            sketch_feature = self.bilstm_network(sketch_feature.unsqueeze(0)) #(1, 25, 64)
+            sketch_feature = self.sketch_embedding_network(sketch_imgs_tensor[i].to(device))
+            sketch_feature = self.sketch_attention(sketch_feature)
+            sketch_features.append(sketch_feature)
             
-            sketch_features.append(F.normalize(sketch_feature, dim=-1))
-            # positive_feature_raw = positive_feature[i].unsqueeze(0) # (1, 64)
-            # negative_feature_raw = negative_feature[i].unsqueeze(0) # (1, 64)
-            
-            # positive_feature_raw = positive_feature_raw.repeat(sketch_feature.shape[0], 1) # (25, 64)
-            # negative_feature_raw = negative_feature_raw.repeat(sketch_feature.shape[0], 1) # (25, 64)
-            # loss += self.loss(sketch_feature, positive_feature_raw, negative_feature_raw)
+        sketch_features = torch.stack(sketch_features, dim=0) # (N, 25, 2048)
+        sketch_features = self.bilstm_network(sketch_features) # (N, 25, 64)
         
-        # loss = self.compute_loss(sketch_features, positive_feature, negative_feature)
-        sketch_features = torch.stack(sketch_features)
-        loss = self.compute_loss(sketch_features, positive_feature, negative_feature)
+        loss = self.loss(sketch_features, positive_feature, negative_feature)
         loss.backward()
         self.optimizer.step()
 
@@ -94,7 +75,7 @@ class BiLSTM_FGSBIR_Model(nn.Module):
         positive_feature = self.linear(self.attention(positive_feature))
         
         # print("positive_feature shape: ", positive_feature.shape)
-        sketch_imgs_tensor = batch['sketch_imgs'] # (N, 25 3, 299, 299)
+        sketch_imgs_tensor = torch.stack(batch['sketch_imgs'], dim=1) # (N, 25 3, 299, 299)
         sketch_features = []
         for i in range(sketch_imgs_tensor.shape[0]):
             sketch_feature = self.sketch_embedding_network(sketch_imgs_tensor[i].to(device))
@@ -114,66 +95,58 @@ class BiLSTM_FGSBIR_Model(nn.Module):
         
         for idx, batch in enumerate(tqdm(dataloader_test)):
             sketch_feature, positive_feature = self.test_forward(batch)
-            sketch_array_tests.extend(sketch_feature)
-            sketch_names.extend(batch['sketch_path'])
+            sketch_array_tests.append(sketch_feature)
+            sketch_names.append(batch['sketch_path'])
             
             for i_num, positive_name in enumerate(batch['positive_path']): 
                 if positive_name not in image_names:
                     image_names.append(batch['positive_sample'][i_num])
                     image_array_tests.append(positive_feature[i_num])
                 
+        sketch_array_tests = torch.stack(sketch_array_tests)
         image_array_tests = torch.stack(image_array_tests)
         
-        # print("sketch_array_tests shape : ", sketch_array_tests.shape) # [323, 1, 25, 2048]
+        # print("sketch_array_tests shape 2: ", sketch_array_tests.shape)
         
-        # sketch_steps = len(sketch_array_tests[0]) # 1
-        # print("sketch_steps: ", sketch_steps) # 1
+        sketch_steps = len(sketch_array_tests[0])
 
         avererage_area = []
         avererage_area_percentile = []
         
-        rank_all = torch.zeros(len(sketch_array_tests))
-        rank_all_percentile = torch.zeros(len(sketch_array_tests))
+        rank_all = torch.zeros(len(sketch_array_tests), sketch_steps)
+        rank_all_percentile = torch.zeros(len(sketch_array_tests), sketch_steps)
         
-        # print("rank_all_percentile shape: ", rank_all_percentile.shape) #(323, 1)
-        for i_batch, sample_batch in enumerate(sketch_array_tests):
+        # print("rank_all_percentile shape: ", rank_all_percentile.shape)
+        for i_batch, sanpled_batch in enumerate(sketch_array_tests):
             mean_rank = []
             mean_rank_percentile = []
-            sketch_name = sketch_names[i_batch]
+            sketch_name = sketch_names[i_batch][0]
             # print(f'sketch_name: {sketch_name}')
             
             sketch_query_name = '_'.join(sketch_name.split('/')[-1].split('_')[:-1])
             position_query = image_names.index(sketch_query_name)
             
-            # print("sample_batch shape: ", sample_batch.shape) # (1, 25, 2048)
-            sample_batch = sample_batch.unsqueeze(0)
-            for i_sketch in range(sample_batch.shape[0]):
-                sketch_feature = self.bilstm_network(sample_batch[i_sketch].unsqueeze(0).to(device))
-                target_distance = F.pairwise_distance(F.normalize(sketch_feature[:, -1, :]).unsqueeze(0).to(device), image_array_tests[position_query].unsqueeze(0).to(device))
-                distance = F.pairwise_distance(F.normalize(sketch_feature[:, -1, :]).unsqueeze(0).to(device), image_array_tests.to(device))
+            print("sanpled_batch shape: ", sanpled_batch.shape)
+            for i_sketch in range(sanpled_batch.shape[0]):
+                sketch_feature = self.bilstm_network(sanpled_batch[:i_sketch+1].to(device))
+                target_distance = F.pairwise_distance(sketch_feature[:, -1, :].unsqueeze(0).to(device), image_array_tests[position_query].unsqueeze(0).to(device))
+                distance = F.pairwise_distance(sketch_feature[:, -1, :].unsqueeze(0).to(device), image_array_tests.to(device))
                 
-                # print("target_distance: ", target_distance)
-                # print("distance: ", distance)
-                # print("i_batch: ", i_batch)
-                # print("i_sketch: ", i_sketch)
-                rank_all[i_batch] = distance.le(target_distance).sum()
-                rank_all_percentile[i_batch] = (len(distance) - rank_all[i_batch]) / (len(distance) - 1)
+                rank_all[i_batch, i_sketch] = distance.le(target_distance).sum()
+                rank_all_percentile[i_batch, i_sketch] = (len(distance) - rank_all[i_batch, i_sketch]) / (len(distance) - 1)
                 
-                
-                if rank_all[i_batch].item() == 0:
-                    mean_rank.append(0.0001)
+                if rank_all[i_batch, i_sketch].item() == 0:
+                    mean_rank.append(1.)
                 else:
-                    mean_rank.append(1/rank_all[i_batch].item())
-                    mean_rank_percentile.append(rank_all_percentile[i_batch].item())
-
+                    mean_rank.append(1/rank_all[i_batch, i_sketch].item())
+                    mean_rank_percentile.append(rank_all_percentile[i_batch, i_sketch].item())
+            
             avererage_area.append(np.sum(mean_rank)/len(mean_rank))
             avererage_area_percentile.append(np.sum(mean_rank_percentile)/len(mean_rank_percentile))
-
         
-        # print("rank_all: ", rank_all)
-        top1_accuracy = rank_all.le(1).sum().numpy() / rank_all.shape[0]
-        top5_accuracy = rank_all.le(5).sum().numpy() / rank_all.shape[0]
-        top10_accuracy = rank_all.le(10).sum().numpy() / rank_all.shape[0]
+        top1_accuracy = rank_all[:, -1].le(1).sum().numpy() / rank_all.shape[0]
+        top5_accuracy = rank_all[:, -1].le(5).sum().numpy() / rank_all.shape[0]
+        top10_accuracy = rank_all[:, -1].le(10).sum().numpy() / rank_all.shape[0]
         
         meanMB = np.mean(avererage_area)
         meanMA = np.mean(avererage_area_percentile)
