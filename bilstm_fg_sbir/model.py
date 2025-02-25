4fc34d361365b100c25f2febba93a49d360e2581
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-02-25 11:28:56 +0700

    update model

--- bilstm_fg_sbir/model.py
+++ bilstm_fg_sbir/model.py
@@ -90,8 +90,10 @@ class BiLSTM_FGSBIR_Model(nn.Module):
             positive_feature_raw = positive_feature[i]
             negative_feature_raw = negative_feature[i]
             
-            positive_feature_raw = positive_feature_raw.repeat(anchor.shape[0])
-            negative_feature_raw = negative_feature_raw.repeat(anchor.shape[0])
+            print("anchor shape:", anchor.shape)
+            print("positive_feature_raw shape:", positive_feature_raw.shape)
+            positive_feature_raw = positive_feature_raw.repeat(anchor.shape[0], 1)
+            negative_feature_raw = negative_feature_raw.repeat(anchor.shape[0], 1)
             
             loss = self.loss(anchor, positive_feature_raw, negative_feature_raw)
             total_loss += loss
