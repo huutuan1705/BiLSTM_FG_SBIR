afdfc80d638e3c6d93f910c14d4df66dfca06b3c
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-01 11:08:24 +0700

    update model

--- bilstm_fg_sbir/model.py
+++ bilstm_fg_sbir/model.py
@@ -117,6 +117,7 @@ class BiLSTM_FGSBIR_Model(nn.Module):
         rank_all = torch.zeros(len(sketch_array_tests)) # (323, 1)
         rank_all_percentile = torch.zeros(len(sketch_array_tests)) # (323, 1)
         
+        t = 0
         # print("rank_all_percentile shape: ", rank_all_percentile.shape)
         for i_batch, sanpled_batch in enumerate(sketch_array_tests):
             sketch_name = sketch_names[i_batch][0]
@@ -130,8 +131,11 @@ class BiLSTM_FGSBIR_Model(nn.Module):
             target_distance = F.pairwise_distance(sketch_feature.unsqueeze(0).to(device), image_array_tests[position_query].unsqueeze(0).to(device))
             distance = F.pairwise_distance(sketch_feature.unsqueeze(0).to(device), image_array_tests.to(device))
             
-            print("target_distance[0]: ", target_distance[0])
-            print("distance[0]: ", distance[0])
+            # print("target_distance[0]: ", target_distance[0])
+            # print("distance[0]: ", distance[0])
+            
+            print("target_distance: ", target_distance)
+            print("distance: ", distance)
             
             rank_all[i_batch] = distance[0].le(target_distance[0]).sum()
             rank_all_percentile[i_batch] = (len(distance[0]) - rank_all[i_batch]) / (len(distance[0]) - 1)
@@ -141,7 +145,10 @@ class BiLSTM_FGSBIR_Model(nn.Module):
             
             avererage_area.append(1/rank_all[i_batch].item() if rank_all[i_batch].item()!=0 else 1)
             avererage_area_percentile.append(rank_all_percentile[i_batch].item() if rank_all_percentile[i_batch].item()!=0 else 1)
-        
+            
+            t += 1
+            if t == 4:
+                break
         print("rank_all: ", rank_all) # 323
         # print("len(rank_all[0]): ", len(rank_all[0])) # 25
         # rank_all, _ = torch.min(rank_all, dim=1)
