5109e6f8146647488b470ed957d1dfef569c5195
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-01 10:13:25 +0700

    update model

--- bilstm_fg_sbir/model.py
+++ bilstm_fg_sbir/model.py
@@ -134,14 +134,14 @@ class BiLSTM_FGSBIR_Model(nn.Module):
             target_distance = F.pairwise_distance(sketch_feature.unsqueeze(0).to(device), image_array_tests[position_query].unsqueeze(0).to(device))
             distance = F.pairwise_distance(sketch_feature.unsqueeze(0).to(device), image_array_tests.to(device))
             
-            print("target_distance: ", target_distance)
-            print("distance: ", distance)
+            # print("target_distance: ", target_distance)
+            # print("distance: ", distance)
             
-            rank_all[i_batch] = distance.le(target_distance).sum()
-            rank_all_percentile[i_batch] = (len(distance) - rank_all[i_batch]) / (len(distance) - 1)
+            rank_all[i_batch] = distance[0].le(target_distance[0]).sum()
+            rank_all_percentile[i_batch] = (len(distance[0]) - rank_all[i_batch]) / (len(distance[0]) - 1)
             
-            print("rank_all[i_batch]: ", rank_all[i_batch])
-            print("rank_all[i_batch].item(): ", rank_all[i_batch].item())
+            # print("rank_all[i_batch]: ", rank_all[i_batch])
+            # print("rank_all[i_batch].item(): ", rank_all[i_batch].item())
             avererage_area.append(1/rank_all[i_batch].item() if rank_all[i_batch].item()!=0 else 1)
             avererage_area_percentile.append(rank_all_percentile[i_batch].item() if rank_all_percentile[i_batch].item()!=0 else 1)
         
