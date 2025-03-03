2d976df6eefc5d4323bc2cd2c74cce790505dbb1
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-03-03 13:49:25 +0700

    update dataset

--- bilstm_fg_sbir/dataset.py
+++ bilstm_fg_sbir/dataset.py
@@ -100,7 +100,9 @@ class FGSBIR_Dataset(Dataset):
             vector_x = self.coordinate[sketch_path]
             list_sketch_imgs = rasterize_sketch(vector_x, self.args.num_anchors)
             if self.on_fly:
-                sketch_imgs = sketch_imgs = torch.stack([self.train_transform(Image.fromarray(sk_img).convert("RGB")) for sk_img in list_sketch_imgs])
+                sketch_imgs = [self.train_transform(Image.fromarray(sk_img).convert("RGB")) for sk_img in list_sketch_imgs]
+                sketch_imgs = torch.cat(sketch_imgs).view(-1, 3, 299, 299)
+                print("sketch_imgs.shape: ", sketch_imgs.shape)
 
             else:
                 sketch_imgs = self.test_transform(Image.fromarray(list_sketch_imgs[-1]).convert("RGB"))
