dc9f7ff84706821a2ba6b05a0ddbf0733d1f6f83
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-05-11 20:59:15 +0700

    update main

--- bilstm_fg_sbir/model.py
+++ bilstm_fg_sbir/model.py
@@ -116,7 +116,7 @@ class BiLSTM_FGSBIR_Model(nn.Module):
                 # print("sampled_batch[:i_sketch+1].shape: ", sampled_batch[:i_sketch+1].shape)
                 sketch_feature = self.bilstm_network(sampled_batch[:i_sketch+1].to(device))
                 target_distance = F.pairwise_distance(F.normalize(sketch_feature.unsqueeze(0)).to(device), image_array_tests[position_query].unsqueeze(0).to(device))
-                distance = F.pairwise_distance(F.normalize(sketch_feature.unsqueeze(0)).to(device), image_array_tests.to(device))
+                distance = F.pairwise_distance(F.normalize(sketch_feature).to(device), image_array_tests.to(device))
                 
                 rank_all[i_batch, i_sketch] = distance.le(target_distance).sum()
 
