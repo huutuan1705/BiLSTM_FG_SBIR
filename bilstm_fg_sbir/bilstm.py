fc78855f332ea771e94e3680c7e51f4ce7f0ea2f
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-06-09 09:10:56 +0700

    update train

--- bilstm_fg_sbir/bilstm.py
+++ bilstm_fg_sbir/bilstm.py
@@ -5,40 +5,44 @@ import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class BiLSTM(nn.Module):
-    def __init__(self, args, input_dim=2048, hidden_dim=1024, output_dim=64, num_layers=2, dropout=0.1):
+    def __init__(self, args, input_dim=2048, hidden_dim=512, output_dim=64, dropout=0.1):
         super(BiLSTM, self).__init__()
 
-        self.bi_lstm = nn.LSTM(
+        self.bi_lstm1 = nn.LSTM(
             input_size=input_dim,
             hidden_size=hidden_dim,
-            num_layers=num_layers,
+            batch_first=True,
+            dropout=dropout,
+            bidirectional=True
+        )
+        
+        self.bi_lstm2 = nn.LSTM(
+            input_size=hidden_dim*2,
+            hidden_size=32,
             batch_first=True,
             dropout=dropout,
             bidirectional=True
         )
 
         # Attention layer
-        self.attention_fc = nn.Linear(hidden_dim * 2, 1)
+        self.attention_fc = nn.Linear(64, 1)
 
-        # Output projection to final embedding space
-        self.output_fc = nn.Linear(hidden_dim * 2, output_dim)
 
     def forward(self, sketch_seq):
         """
         sketch_seq: Tensor of shape (batch_size, seq_len, input_dim)
         """
         # Bi-LSTM output
-        lstm_out, _ = self.bi_lstm(sketch_seq)  # shape: (batch, seq_len, hidden_dim*2)
+        lstm_out1, _ = self.bi_lstm1(sketch_seq)  # shape: (batch, seq_len, hidden_dim*2)
+        lstm_out2, _ = self.bi_lstm2(lstm_out1)
 
         # Compute attention scores
-        attn_scores = self.attention_fc(lstm_out)  # (batch, seq_len, 1)
+        attn_scores = self.attention_fc(lstm_out2)  # (batch, seq_len, 1)
         attn_weights = torch.softmax(attn_scores, dim=1)  # normalize over sequence length
 
-        context_vector = attn_weights * lstm_out  # (batch, seq_len, hidden_dim*2)
+        context_vector = attn_weights * lstm_out2  # (batch, seq_len, hidden_dim*2)
 
-        # Final projection
-        output = self.output_fc(context_vector)  # (batch, output_dim)
-        return F.normalize(output)
+        return F.normalize(context_vector)
 
     
 # class BiLSTM(nn.Module):
@@ -60,7 +64,7 @@ class BiLSTM(nn.Module):
 #         # x = F.normalize(x)
 #         return x 
     
-# model = BiLSTM()
+# model = BiLSTM(None)
 # x = torch.randn(3, 25, 2048)
 # x = model(x)
 # print(x.shape) #(3, 25, 64)
